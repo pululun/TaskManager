967421e9e09017ca71503773427dd5c4373eee0d
kapcap4 <kapcap43@gmail.com>
2017-11-24 21:46:13 +0400

    realize class Task

--- src/es/esy/kapcapx/Task.java
+++ src/es/esy/kapcapx/Task.java
@@ -1,4 +1,50 @@
 package es.esy.kapcapx;
 
-public class Task {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Task implements Serializable{
+    private String title;
+    private String description;
+    private Date date;
+    private String contacts;
+
+    Task (String title, String description, Date date, String contacts) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.contacts = contacts;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setContacts(String contacts) {
+        this.contacts = contacts;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getContacts() {
+        return contacts;
+    }
 }
